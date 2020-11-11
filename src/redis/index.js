02701f79dc20d8cadc5bae6d13c7657444b506be
Samir Shrestha <theonlysamir@gmail.com>
2020-11-11 22:37:43 +0545

    Redis Port Value Pushed To Env

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,5 +1,5 @@
 const redis = require("redis");
-const client = redis.createClient(6379);
+const client = redis.createClient(process.env.REDIS_PORT);
 
 client.on("error", function(error) {
     console.error(error);
